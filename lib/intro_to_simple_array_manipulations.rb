964eab87a422a4154c905aff4d56af94d3c65e0c
Jess Rezac <jess.rezac@gmail.com>
2019-04-22 01:38:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,3 +24,24 @@ end
 
 def using_concat(first_array, second_array)
   first_array.concat(second_array)
+end
+
+def using_insert(array, new_element)
+  array.insert(4, new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
